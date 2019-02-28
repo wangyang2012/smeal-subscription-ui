469844b394898d904a220c57c231f40da10281d8
wangyang2012 <wangyang1712@gmail.com>
2019-02-28 03:39:29 +0100

    add files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CreateSubscriptionComponent } from './create-subscription/create-subscription.component';
+import {AppRoutingModule} from './app-routing.module';
+import {SubscriptionService} from './service/subscription.service';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateSubscriptionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+
+  providers: [SubscriptionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
