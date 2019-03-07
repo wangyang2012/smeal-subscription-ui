793973707158c1ae4d9f7207608d808f149a59bd
wangyang2012 <wangyang1712@gmail.com>
2019-03-07 14:14:42 +0100

    create subscription

--- src/app/service/subscription.service.ts
+++ src/app/service/subscription.service.ts
@@ -6,6 +6,7 @@ import {KeyValue} from '../model/KeyValue';
 @Injectable()
 export class SubscriptionService {
   private url = 'http://54.38.189.20:8080/subscription';
+  // private url = 'http://localhost:9090';
   constructor(private http: HttpClient) { }
 
   getCart(cartId: number) {
@@ -13,5 +14,9 @@ export class SubscriptionService {
   }
   getCustomer(customerId: number) {
     return this.http.get<KeyValue>(this.url + '/customer/' + customerId);
+  }
+
+  createSubscription(token: string) {
+    return this.http.post<string>(this.url + '/subscription/create', token);
   }
 }
